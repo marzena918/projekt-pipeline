492bbcc2e95762489a2a56b5e050250de70456c0
marzenusia <nsnjjs@com.pl>
2024-02-12 23:13:03 +0100

    zmiana projektu

---
+++ src/main/java/com/example/Wool_Amore/WoolAmoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Wool_Amore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoolAmoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoolAmoreApplication.class, args);
+	}
+
+}
